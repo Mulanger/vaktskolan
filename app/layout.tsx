cb298b74331097bcb19d2ef0b044ac86d7a3e139
Kevin <mulengakevin@gmail.com>
2026-07-16 16:21:55 +0200

    Lägg till Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Hanken_Grotesk, Inter } from "next/font/google";
+import Script from "next/script";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { metadataBase } from "@/lib/seo";
@@ -36,6 +37,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <SiteHeader />
         {children}
         <SiteFooter />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGHQ62P8QY"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FGHQ62P8QY');
+          `}
+        </Script>
       </body>
     </html>
   );
